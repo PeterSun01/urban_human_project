2b5262d9bdf7c2e691361d182a07b5e69bab8086
SunPeter <15041121245@163.com>
2019-02-28 16:30:41 +0800

    first

---
+++ components/Human/include/Human.h
@@ -0,0 +1,16 @@
+
+#ifndef _HUMAN_H_
+#define _HUMAN_H_
+
+extern void Human_Init(void);
+extern void Humanapp(void);
+
+#define GPIO_HUMAN      14
+
+int havehuman_count;
+int nohuman_count;
+
+int need_send;
+
+#endif
+
